46dc1e8657a2e285c7180b8f7de5e41eb0c501bd
Tomas Tomecek <ttomecek@redhat.com>
2019-10-21 17:55:08 +0200

    buildah get version: add rhel 8 test case
    Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

--- tests/integration/test_buildah.py
+++ tests/integration/test_buildah.py
@@ -1,7 +1,22 @@
 import pytest
+from flexmock import flexmock
 
+from ansible_bender.builders import buildah_builder
 from ansible_bender.builders.buildah_builder import BuildahBuilder
 from ansible_bender.conf import Build
+
+
+BUILDAH_15_VERSION = """ \
+Version:         1.5
+Go Version:      go1.11.5
+Image Spec:      1.0.0
+Runtime Spec:    1.0.0
+CNI Spec:        0.4.0
+libcni Version:
+Git Commit:
+Built:           Thu Jan  1 00:00:00 1970
+OS/Arch:         linux/amd64
+"""
 
 
 @pytest.mark.parametrize("image_name,found", [
@@ -26,3 +41,11 @@ def test_get_version():
     b = BuildahBuilder(Build(), debug=True)
     version = b.get_buildah_version()
     assert [x for x in version if isinstance(x, int)]
+
+
+def test_get_version_rhel_8():
+    flexmock(buildah_builder, run_cmd=lambda *args, **kwargs: BUILDAH_15_VERSION)
+    b = BuildahBuilder(Build(), debug=True)
+    version = b.get_buildah_version()
+    assert [x for x in version if isinstance(x, int)]
+    assert version < (1, 7, 3)
